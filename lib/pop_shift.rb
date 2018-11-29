53a01660d862cf5cd8c893666c2703b513f43ed5
brickstar <brickstar@gmail.com>
2018-11-28 20:40:32 -0700

    Cleanup

--- lib/pop_shift.rb
+++ lib/pop_shift.rb
@@ -3,15 +3,15 @@ class PopShift
   def popshift(string)
     return [string, "", ""] if string.length == 1
     if string.length.odd?
-      first = string.slice(string.slice(((string.length / 2) + 1)..string.length)).reverse
-      second = string.slice(0..string.length/2)
+      first = string.slice(string.slice((string.length / 2) + 1..string.length)).reverse
+      second = string.slice(0..string.length /  2)
       second = second.chars
       third = second.pop
       [first, second.join, third]
     else
-      second = string[0, string.length/2]
+      second = string[0, string.length / 2]
       first = string.chars
-      first = first[-(first.length/2), first.length/2].join.reverse
+      first = first[-(first.length / 2), first.length/2].join.reverse
       [first, second, ""]
     end
   end
